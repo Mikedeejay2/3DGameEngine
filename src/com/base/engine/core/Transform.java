4d305d7edeb5d328ee8fb6133419fa61a519a29c
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2020-02-01 16:46:07 -0500

    Actually used quaternions.

--- src/com/base/engine/core/Transform.java
+++ src/com/base/engine/core/Transform.java
@@ -3,20 +3,20 @@ package com.base.engine.core;
 public class Transform
 {
     private Vector3f pos;
-    private Vector3f rot;
+    private Quaternion rot;
     private Vector3f scale;
 
     public Transform()
     {
         pos = new Vector3f(0, 0, 0);
-        rot = new Vector3f(0, 0, 0);
+        rot = new Quaternion(0, 0, 0, 1);
         scale = new Vector3f(1, 1, 1);
     }
 
     public Matrix4f getTransformation()
     {
         Matrix4f translationMatrix = new Matrix4f().initTranslation(pos.getX(), pos.getY(), pos.getZ());
-        Matrix4f rotationMatrix = new Matrix4f().initRotation(rot.getX(), rot.getY(), rot.getZ());
+        Matrix4f rotationMatrix = rot.toRotationMatrix();
         Matrix4f scaleMatrix = new Matrix4f().initScale(scale.getX(), scale.getY(), scale.getZ());
 
         return translationMatrix.mul(rotationMatrix.mul(scaleMatrix));
@@ -44,19 +44,19 @@ public class Transform
         this.pos = new Vector3f(x, y, z);
     }
 
-    public Vector3f getRot()
+    public Quaternion getRot()
     {
         return rot;
     }
 
-    public void setRot(Vector3f rot)
+    public void setRot(Quaternion rot)
     {
         this.rot = rot;
     }
 
-    public void setRotation(float x, float y, float z)
+    public void setRot(float x, float y, float z, float w)
     {
-        this.rot = new Vector3f(x, y, z);
+        this.rot = new Quaternion(x, y, z, w);
     }
 
     public Vector3f getScale()
